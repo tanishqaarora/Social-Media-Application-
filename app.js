cb64f7cda675d7759a85fd60f332fe72388e17ee
Tanishqa Arora <tanishqa164@gmail.com>
2022-12-08 23:58:24 +0530

    add post api route

--- app.js
+++ app.js
@@ -19,15 +19,19 @@ app.use(session({
     saveUninitialized: false
 }))
 
-//Routes
+// Routes
 const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
 const logoutRoute = require('./routes/logout');
 
+// Api routes
+const postsApiRoute = require('./routes/api/posts');
 
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 app.use('/logout', logoutRoute);
+
+app.use('/api/posts', postsApiRoute);
 
 app.get('/', middleware.requireLogin, (req, res, next)=>{
 
