607bf81336dad8557f47f108bce3c74536c59f5c
Tanishqa Arora <tanishqa164@gmail.com>
2022-12-10 00:12:30 +0530

    outputting the user info from the post

--- public/js/common.js
+++ public/js/common.js
@@ -23,7 +23,40 @@ $('#submitPostButton').click(() => {
         content: textbox.val()
     }
 
-    $.post('/api/posts', data, (postData, status, xhr) => {
-        console.log(postData);
+    $.post('/api/posts', data, postData => {
+        
+        var html = createPostHtml(postData);
+        $('.postsContainer').prepend(html);
+        textbox.val('');
+        button.prop('disabled', true);
+    
     })
 })
+
+function createPostHtml(postData) {
+
+    var postedBy = postData.postedBy;
+    var displayName = postedBy.firstName + " " + postedBy.lastName; 
+    var timestamp = postData.createdAt;
+
+    return `<div class='post'>
+    
+                <div class='mainContentContainer'>
+                    <div class='userImageContainer'>
+                        <img src='${postedBy.profilePic}'> 
+                    </div>
+                    <div class='postContentContainer>
+                        <div class='header'>
+                            <a href='/profile/${postedBy.username}'>${displayName}</a>
+                            <span class='username'>@${postedBy.username}</span>
+                            <span class='date'>${timestamp}</span>
+                        </div>
+                        <div class='postBody'>
+                            <span>${postData.content}</span>
+                        </div>
+                        <div class='postFooter'>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
