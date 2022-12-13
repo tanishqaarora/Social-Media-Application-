a98902c5b765b2f88c67396bd88609e16a9aa1d1
Tanishqa Arora <tanishqa164@gmail.com>
2022-12-13 23:50:29 +0530

    dotenv config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const middleware = require('./middleware');
 const path = require('path');
 const bodyParser = require('body-parser');
