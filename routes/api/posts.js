8c54296e73ee3416e9e9e4dd5aed53f222ecc913
Tanishqa Arora <tanishqa164@gmail.com>
2022-12-20 01:56:22 +0530

    refactor the get posts function

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,19 +7,9 @@ const Post = require('../../schemas/PostSchema');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {
-    Post.find()
-    .populate("postedBy")
-    .populate("retweetData")
-    .sort({ "createdAt": -1 })
-    .then(async results => {
-        results = await User.populate( results, {path: "retweetData.postedBy"});
-        res.status(200).send(results);
-    })
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    });
+router.get("/", async (req, res, next) => {
+    var results = await getPosts();
+    res.status(200).send(results);
 })
 
 router.get("/:id", (req, res, next) => {
@@ -120,5 +110,14 @@ router.post("/:id/retweet", async (req, res, next) => {
     return res.status(200).send(post);
 })
 
+async function getPosts() {
+    var results = await Post.find()
+    .populate("postedBy")
+    .populate("retweetData")
+    .sort({ "createdAt": -1 })
+    .catch(error =>  console.log(error))
+
+    return await User.populate( results, {path: "retweetData.postedBy"}); 
+}
 
 module.exports = router;
