90cbba23dc73534477f4cecbc3cbd5bb9d5400a4
Tanishqa Arora <tanishqa164@gmail.com>
2023-01-21 00:24:22 +0530

    correction in profile page and include/exclude the replies

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
 });
 
 function loadPosts() {
-    $.get("/api/posts", { postedBy: profileUserId}, results => {
+    $.get("/api/posts", { postedBy: profileUserId, isReply: true}, results => {
         outputPosts(results, $(".postsContainer"));
     })
 }
