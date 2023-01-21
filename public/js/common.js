3d9d5a4f738487576ed1010b6d47a4bb3389f75c
Tanishqa Arora <tanishqa164@gmail.com>
2023-01-21 21:38:29 +0530

    updatation in follower and following

--- public/js/common.js
+++ public/js/common.js
@@ -147,17 +147,30 @@ $(document).on("click", ".followButton", (e) => {
     $.ajax({
         url: `/api/users/${userId}/follow`,
         type: "PUT",
-        success: (data) => {
-            console.log(data);
-            // button.find("span").text(postData.retweetUsers.
-            // length || "");
+        success: (data, status, xhr) => {
+            // user not found
+            if(xhr.status == 404) {
+                alert("user not found");
+                return;
+            }
 
-            // if(postData.retweetUsers.includes(userLoggedIn._id)) {
-            //     button.addClass("active");
-            // }
-            // else {
-            //     button.removeClass("active");
-            // }
+            var difference = 1;
+            if(data.following && data.following.includes(userId)) {
+                button.addClass("following");
+                button.text("Following");
+            }
+            else {
+                button.removeClass("following");
+                button.text("Follow");
+                difference = -1;
+            }
+
+            var followersLabel = $("#followersValue");
+            if(followersLabel.lenght != 0) {
+                var followersText = followersLabel.text();
+                followersText = parseInt(followersText);
+                followersLabel.text(followersText + difference)
+            }
 
         }
     })
